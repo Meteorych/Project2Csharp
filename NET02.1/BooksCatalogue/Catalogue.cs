4df74246db1959956cb721719ffc0687ad0e261e
Meteorych <super.titlov@inbox.ru>
2023-09-19 13:15:56 +0300

    IEnumerator

--- NET02.1/BooksCatalogue/Catalogue.cs
+++ NET02.1/BooksCatalogue/Catalogue.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BooksCatalogue
 {
-    internal class Catalogue
+    class Catalogue : IEnumerable<Book>
     {
+        private List<Book> _books = new();
+        public Catalogue(List<Book> books)
+        {
+            _books = books;
+        }
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+        public Book? GetBook(string isbn) 
+        {
+            foreach (Book book in _books)
+            {
+                if (book.ISBN == isbn) return book;
+            }
+            return null;
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            var sortedBooks = _books.OrderBy(book => book.Name).ToList();
+
+            foreach (var book in sortedBooks)
+            {
+                yield return book;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
     }
 }
