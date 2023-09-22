ec9f2fb3da2c9dcde250c197d7665c18e1f6d0ad
Meteorych <super.titlov@inbox.ru>
2023-09-22 16:50:34 +0300

    Tests

--- NET02.1/TestProject1/UnitTest1.cs
+++ NET02.1/TestProject1/UnitTest1.cs
@@ -20,5 +20,38 @@ namespace TestProject1
             Author testAuthor1 = new($"{RandomStringCreation.RandomString(205)}", $"{RandomStringCreation.RandomString(205)}");
             Assert.AreEqual(excpectedLength, testAuthor1.LastName.Length);
         }
+        [TestMethod]
+        public void ReleaseDateParsingTest () 
+        {
+            DateOnly expected = new(2003, 9, 23);
+            string releaseDate = "2003.09.23";
+            Book testBook = new Book("1234567890123", "Title", releaseDate);
+            Assert.AreEqual (expected, testBook.ReleaseDate);
+        }
+        [TestMethod]
+        public void LinqToObjectCatalogueBookTimeTest()
+        {
+
+        }
+        [TestMethod]
+        public void LinqToObjectCatalogueBookNameTest() 
+        { 
+        
+        }
+        [TestMethod]
+        public void LinqToObjectCatalogueTupleAuthorBookCountTest()
+        {
+            
+        }
+        [TestMethod]
+        public void CatalogueEnumanatorTest()
+        {
+
+        }
+        [TestMethod]
+        public void GetBookByISBNTest()
+        {
+
+        }
     }
 }
