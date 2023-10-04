d23140ff660ebaf21c897aa7f32c13a460e948f9
Meteorych <super.titlov@inbox.ru>
2023-10-04 18:23:00 +0300

    Some model

---
+++ NET01.2/TemplatesDelegatesEvents/Matrixes/Matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatesDelegatesEvents.Matrixes
+{
+    /// <summary>
+    /// Класс-родитель для представления матрицы
+    /// </summary>
+    abstract class Matrix<T>
+    {
+        public delegate void MatrixHandler(int[] indexes, T oldValue);
+        public event MatrixHandler chan;
+    }
+}
