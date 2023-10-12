5484fa035cfd2cfcc837f80d64f8e86988566674
Meteorych <super.titlov@inbox.ru>
2023-10-12 08:56:55 +0300

    Changes

---
+++ NET01.1/Interfaces/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Interfaces
+{
+    internal abstract class BaseEntity
+    {
+        public Guid Id { get; set;}
+        public string? Description { get; set; }
+    }
+}
---
+++ NET01.1/TrainingMaterials/TextTrainingMaterial.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1.TrainingMaterials
+{
+    internal class TextTrainingMaterial : TrainingMaterial
+    {
+        public string Text { get;}
+        private const int maxLength = 10000;
+        public TextTrainingMaterial(string text, string? description)
+        {
+            if (string.IsNullOrEmpty(text)) throw new ArgumentNullException();
+            if (text.Length > maxLength)
+            {
+                Text = text[..maxLength];
+            }
+            else
+            {
+                Text = text;
+            }
+            Id = Guid.NewGuid();
+            Description = description;
+        }
+    }
+}
