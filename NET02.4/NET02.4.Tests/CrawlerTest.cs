88b54efab310a46316cee4f851e48cb35ae9161e
Ivan Titlov <super.titlov@inbox.ru>
2024-01-29 20:00:17 +0300

    One test with mistake

--- NET02.4/NET02.4.Tests/CrawlerTest.cs
+++ NET02.4/NET02.4.Tests/CrawlerTest.cs
@@ -1,20 +1,37 @@
+using System.Runtime.InteropServices.JavaScript;
 using Microsoft.Extensions.Configuration;
+using Moq;
+using NET02._4.Crawler;
 using NLog;
 
 namespace NET02._4.Tests
 {
     public class UnitTest1
     {
-        private readonly IConfiguration _config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
-        private readonly Logger _logger = LogManager.GetLogger("Crawling Logger");
+        private readonly Mock<ILogger> _logger = new();
 
         [Fact]
         public void Test1()
         {
+            var config = Mock.Of<IConfiguration>(
+                config => config["Url"] == "https://www.youtube.com" &&
+                          config["Timeout"] == "00:00:02" &&
+                          config["MaxWaitingTime"] == "00:00:03" &&
+                          config["MailAddress"] == "super.titlov@inbox.ru" &&
+                          config["AdministratorName"] == "Ivan Titlov");
+            var crawler = new WebCrawler(config, _logger.Object);
+
+            //Act
+            var startTime = DateTime.Now;
+            do
+            {
+                crawler.Start();
+            } while ((DateTime.Now - startTime).Milliseconds < 100);
+            crawler.Stop();
+
             //TODO: Write tests
+
+            _logger.Verify(logger => logger.Info(It.IsAny<string>()), Times.AtLeastOnce);
         }
     }
 }
